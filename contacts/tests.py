095c3f32d928f874e54d27768de601f345cc9615
Kracekumar Ramaraju <me@kracekumar.com>
2014-06-07 10:16:39 +0530

    Added tests

--- contacts/tests.py
+++ contacts/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .models import Person
+
+
+class PersonTestCase(TestCase):
+    def setUp(self):
+        Person.objects.create(first_name="krace", phone_no=123)
+
+    def test_phone_no(self):
+        p = Person.objects.get(first_name="krace")
+        self.assertEquals(p.phone_no, u'123')
